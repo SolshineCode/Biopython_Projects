d7bf69f0f3c52e814b20e733083d9702b9739bb0
SolshineCode <143902425+SolshineCode@users.noreply.github.com>
2023-11-02 16:36:46 -0700

    Add files via upload
    Corrected references to config in the functions

--- Bioinformatics_FASTA_Sequence_or_Identifiers_Functions.py
+++ Bioinformatics_FASTA_Sequence_or_Identifiers_Functions.py
@@ -3,18 +3,21 @@
 # pip install biopython
 from Bio import SeqIO
 
+import os
+from Bio import SeqIO
+
 Config = {
-    filename: # fill in filename here ie "data/sequences.fasta"
+    "filename": "data/sequences.fasta"
 }
 
 # Read the sequences from the FASTA file
-def read_fasta_file(filename):
-    with open(filename, "r") as file:
+def read_fasta_file():
+    with open(Config["filename"], "r") as file:
         sequences = [str(record.seq) for record in SeqIO.parse(file, "fasta")]
     return sequences
 
 # Read the identifiers from the FASTA file
-def read_fasta_identifiers(filename):
-    with open(filename, "r") as file:
+def read_fasta_identifiers():
+    with open(Config["filename"], "r") as file:
         identifiers = [record.id for record in SeqIO.parse(file, "fasta")]
     return identifiers
